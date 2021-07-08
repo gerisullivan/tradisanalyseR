a9522c9f7ccb07820989f0d1ce5917541252b2b9
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2021-07-08 11:25:04 +1000

    update

--- R/promoter_weblogo.R
+++ R/promoter_weblogo.R
@@ -19,11 +19,38 @@ promoter_weblogo <- function(x, promoters, save_path, save_fasta = FALSE){
     data2 <- data2[order(data2[,4], decreasing = TRUE),]
     data2$newname <- paste0(data2$gene, "_", data2$ins)
     data2 <- data2[!is.na(data2$promoter),]
+    data_inc <- subset(data2, data2$ins == "inc")
+    data_dec <- subset(data2, data2$ins == "dec")
     name <- gsub(pattern = "_logFC", replacement = "", colnames(data)[4])
-    RWebLogo::weblogo(as.character(data2$promoter), file.out = paste0(save_path, name, "_weblogo.png"), open = FALSE, format = "png",
-            title = name, annotate = c(-50,"","","","",-45,"","","","",-40,"","","","",-35,
-                                       "","","","",-30,"","","","",-25,"","","","",-20,"","",
-                                       "","",-15, "","","","",-10,"","","","",-5,"","","",-1), xlabel = "position relative to TSS")
+    p1 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data2$promoter), 1, 25),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE)
+    p1$scales$scales[[1]] <- scale_x_continuous(breaks= seq(1,25,by=5),labels=c("-50", "-45", "-40", "-35", "-30"))
+    p2 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data2$promoter), 26, 50),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE) +
+      labs(x = "Position Relative to TSS")
+    p2$scales$scales[[1]] <- scale_x_continuous(breaks= c(seq(1,25,by=5), 25),labels=c("-25", "-20", "-15", "-10", "-5", "-1"))
+    p3 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data_inc$promoter), 1, 25),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE)
+    p3$scales$scales[[1]] <- scale_x_continuous(breaks= seq(1,25,by=5),labels=c("-50", "-45", "-40", "-35", "-30"))
+    p4 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data_inc$promoter), 26, 50),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE) +
+      labs(x = "Position Relative to TSS")
+    p4$scales$scales[[1]] <- scale_x_continuous(breaks= c(seq(1,25,by=5), 25),labels=c("-25", "-20", "-15", "-10", "-5", "-1"))
+    p5 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data_dec$promoter), 1, 25),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE)
+    p5$scales$scales[[1]] <- scale_x_continuous(breaks= seq(1,25,by=5),labels=c("-50", "-45", "-40", "-35", "-30"))
+    p6 <- ggplot() +
+      ggseqlogo::geom_logo(substring(toupper(data_dec$promoter), 26, 50),
+                           seq_type = "dna", method = "probability", rev_stack_order = TRUE) +
+      labs(x = "Position Relative to TSS")
+    p6$scales$scales[[1]] <- scale_x_continuous(breaks= c(seq(1,25,by=5), 25),labels=c("-25", "-20", "-15", "-10", "-5", "-1"))
+    gridExtra::grid.arrange(p1,p3,p5,p2,p4,p6, nrow = 2, top = name)
+    gridExtra::grid.arrange(p1,p2, nrow = 2, top = name)
     if (save_fasta == TRUE){
     write.fasta(as.list(data2$promoter), as.vector(data2$newname), file.out = paste0(save_path, name, ".fa"), as.string = FALSE, )
     }
