ff44a24717eebf474ee403bb1547ca50bd71fc6c
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2021-07-26 15:13:32 +1000

    update look of operon plots

--- R/get_operons.R
+++ R/get_operons.R
@@ -9,11 +9,11 @@
 #'
 #' @export
 get_operons <- function(logfcs, x, abslogfc = 0, sig = 0.05){
-ectable <- x[,c(1,3)]
-colnames(ectable) <- c("gene", "operon")
-operons <- tidyr::separate_rows(ectable, operon, sep = " // ", convert = TRUE)
+  ectable <- x[,c(1,3)]
+  colnames(ectable) <- c("gene", "operon")
+  operons <- tidyr::separate_rows(ectable, operon, sep = " // ", convert = TRUE)
 
-for (i in seq(4, ncol(logfcs), by = 2)){
+  for (i in seq(4, ncol(logfcs), by = 2)){
   data <- logfcs[,c(1:3, i, i+1)]
   sub <- subset(data, data[,5] < sig)
   sub <- subset(sub, abs(sub[,4]) > abslogfc)
@@ -27,13 +27,17 @@ for (i in seq(4, ncol(logfcs), by = 2)){
   mergeFC <- mergeFC %>% group_by(sig_gene) %>% filter(n()>1)
   mergeFC <- as.data.frame(mergeFC[!is.na(mergeFC[,5]),])
   mergeFC <- subset(mergeFC, !(as.character(mergeFC$gene) == as.character(mergeFC$sig_gene) & mergeFC$sig == "Insignificant"))
-  paste0("Preparing plot for ", gsub("_logFC", replacement = "", colnames(data)[4]))
+  mergeFC <- mergeFC[!isUnique(mergeFC$sig_gene),]
+  print(paste0("Preparing plot for ", gsub("_logFC", replacement = "", colnames(data)[4])))
   p <- ggplot(mergeFC, aes(x = gene, y = mergeFC[,5], group = gene, fill = sig)) +
     geom_bar(stat = "identity", width = 0.7) +
-    scale_fill_manual(values = c("azure4", "mediumseagreen", "coral")) +
+    scale_fill_manual(values = c("gray20", "green4", "red")) +
     facet_wrap(~sig_gene, scales = "free") +
-    theme(axis.text.x = element_text(angle = 25)) +
-    theme(plot.title = element_text(hjust = 0.5)) +
+    theme_light() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 0.95),
+          plot.title = element_text(hjust = 0.5, size = 20),
+          strip.background = element_rect(colour = "black", fill = "gray90"),
+          strip.text = element_text(color = "black", size = 12)) +
     labs(x = "Gene", title = gsub("_logFC", replacement = "", colnames(data)[4]), y = "Log2 Fold Change")
   print(p)
 }
